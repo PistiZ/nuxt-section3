487852146a2c899849a4f118e9503355483f3f93
Balogh István <istvan.balogh@neuron.hu>
2018-11-15 15:07:53 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Working on the Post page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="post-list">
+        <PostPreview id="1"
+                     thumbnail="https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg"
+                     title="Hello there!"
+                     previewText="This is my first post!" />
+
+        <PostPreview id="2"
+                     thumbnail="https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg"
+                     title="Hello there the second time!"
+                     previewText="This is my second post!" />
+
+        <PostPreview id="3"
+                     thumbnail="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/ferrari-488-gtb-rt-2016-web-0032.jpg?itok=96_ZHnuN"
+                     title="Hello there the third time"
+                     previewText="This is my third post!" />
+    </section>
+</template>
+
+<script>
+    import PostPreview from "@/components/Posts/PostPreview";
+
+    export default {
+        name: "PostList",
+        components: {
+            PostPreview
+        }
+    }
+</script>
+
+<style scoped>
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,31 +4,16 @@
             <h1>Get the latest tech news!</h1>
         </section>
 
-        <section class="featured-posts">
-            <PostPreview id="1"
-                         thumbnail="https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg"
-                         title="Hello there!"
-                         previewText="This is my first post!" />
-
-            <PostPreview id="2"
-                         thumbnail="https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg"
-                         title="Hello there the second time!"
-                         previewText="This is my second post!" />
-
-            <PostPreview id="3"
-                         thumbnail="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/ferrari-488-gtb-rt-2016-web-0032.jpg?itok=96_ZHnuN"
-                         title="Hello there the third time"
-                         previewText="This is my third post!" />
-        </section>
+        <PostList />
     </div>
 </template>
 
 <script>
-    import PostPreview from '@/components/Posts/PostPreview'
+    import PostList from "@/components/Posts/PostList";
 
     export default {
         components: {
-            PostPreview
+            PostList
         }
     }
 </script>
@@ -62,14 +47,5 @@
         .intro h1 {
             font-size: 2rem;
         }
-    }
-
-    .featured-posts {
-        display: flex;
-        padding: 20px;
-        box-sizing: border-box;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
     }
 </style>
