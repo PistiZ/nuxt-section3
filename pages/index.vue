f08aa04fffcc50d1454d05e64d1180d1a3d63858
Balogh István <istvan.balogh@neuron.hu>
2018-11-16 15:32:00 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Preparing data on the server

--- pages/index.vue
+++ pages/index.vue
@@ -15,23 +15,33 @@
         components: {
             PostList
         },
-        data() {
-            return {
-                loadedPosts: [
-                    {
-                        id: '1',
-                        title: 'First post',
-                        previewText: 'This is our first post',
-                        thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
-                    },
-                    {
-                        id: '2',
-                        title: 'Second post',
-                        previewText: 'This is our second post',
-                        thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
-                    }
-                ]
-            }
+        // data() {
+        //     return {
+        //         loadedPosts: []
+        //     };
+        // },
+        asyncData(context, callback) {
+            setTimeout(() => {
+                callback(null, {
+                    loadedPosts: [
+                        {
+                            id: '1',
+                            title: 'First post',
+                            previewText: 'This is our first post',
+                            thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
+                        },
+                        {
+                            id: '2',
+                            title: 'Second post',
+                            previewText: 'This is our second post',
+                            thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
+                        }
+                    ]
+                });
+            }, 2000);
+        },
+        created() {
+
         }
     }
 </script>
