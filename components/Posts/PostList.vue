8d43821af2e6873194fa66461a72a93a1d0be69b
Balogh István <istvan.balogh@neuron.hu>
2018-11-16 12:57:27 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Preparing dynamic data

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,22 +1,12 @@
 <template>
     <section class="post-list">
-        <PostPreview id="1"
+        <PostPreview v-for="post in posts"
+                     :key="post.id"
+                     :id="post.id"
                      :is-admin="isAdmin"
-                     thumbnail="https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg"
-                     title="Hello there!"
-                     previewText="This is my first post!" />
-
-        <PostPreview id="2"
-                     :is-admin="isAdmin"
-                     thumbnail="https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg"
-                     title="Hello there the second time!"
-                     previewText="This is my second post!" />
-
-        <PostPreview id="3"
-                     :is-admin="isAdmin"
-                     thumbnail="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/ferrari-488-gtb-rt-2016-web-0032.jpg?itok=96_ZHnuN"
-                     title="Hello there the third time"
-                     previewText="This is my third post!" />
+                     :thumbnail="post.thumbnail"
+                     :title="post.title"
+                     :previewText="post.previewText" />
     </section>
 </template>
 
@@ -32,6 +22,10 @@
             isAdmin: {
                 type: Boolean,
                 default: false
+            },
+            posts: {
+                type: Array,
+                required: true
             }
         }
     }
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
             <h1>Get the latest tech news!</h1>
         </section>
 
-        <PostList />
+        <PostList :posts="loadedPosts" />
     </div>
 </template>
 
@@ -14,6 +14,24 @@
     export default {
         components: {
             PostList
+        },
+        data() {
+            return {
+                loadedPosts: [
+                    {
+                        id: '1',
+                        title: 'First post',
+                        previewText: 'This is our first post',
+                        thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
+                    },
+                    {
+                        id: '2',
+                        title: 'Second post',
+                        previewText: 'This is our second post',
+                        thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
+                    }
+                ]
+            }
         }
     }
 </script>
