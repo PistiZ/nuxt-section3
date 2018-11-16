786a93e4147134337c8ab7b1786c8d7f29e7cbcc
Balogh István <istvan.balogh@neuron.hu>
2018-11-16 16:58:58 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Vuex, fetch() and nuxtServerInit()

--- store/index.js
+++ store/index.js
@@ -11,6 +11,28 @@ const createStore = () => {
             }
         },
         actions: {
+            nuxtServerInit(vuexContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit('setPosts', [
+                            {
+                                id: '1',
+                                title: 'First post',
+                                previewText: 'This is our first post',
+                                thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
+                            },
+                            {
+                                id: '2',
+                                title: 'Second post',
+                                previewText: 'This is our second post',
+                                thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
+                            }
+                        ]);
+
+                        resolve();
+                    }, 1000);
+                });
+            },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts);
             }
