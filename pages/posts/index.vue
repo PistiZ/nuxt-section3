482e061ad1cf847783abb8933954ade1cc66cff9
Balogh István <istvan.balogh@neuron.hu>
2018-11-16 16:05:53 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Using Promise in asyncData

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,25 +11,31 @@
         components: {
             PostList
         },
-        asyncData(context, callback) {
-            setTimeout(() => {
-                callback(null, {
-                    loadedPosts: [
-                        {
-                            id: '1',
-                            title: 'First post',
-                            previewText: 'This is our first post',
-                            thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
-                        },
-                        {
-                            id: '2',
-                            title: 'Second post',
-                            previewText: 'This is our second post',
-                            thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
-                        }
-                    ]
-                });
-            }, 3000);
+        asyncData(context) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve({
+                        loadedPosts: [
+                            {
+                                id: '1',
+                                title: 'First post',
+                                previewText: 'This is our first post',
+                                thumbnail: 'https://cdn.bmwblog.com/wp-content/uploads/2017/05/2017-BMW-M550d-G30-Quadturbo-Diesel-M-Performance-01-830x553.jpg'
+                            },
+                            {
+                                id: '2',
+                                title: 'Second post',
+                                previewText: 'This is our second post',
+                                thumbnail: 'https://picolio.auto123.com/13photo/audi/2013-audi-a4-20-tsfi-fronttrack_1.jpg'
+                            }
+                        ]
+                    });
+                }, 3000);
+            }).then(data => {
+                return data;
+            }).catch(e => {
+                context.error(new Error());
+            });
         }
     }
 </script>
