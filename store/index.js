1eae2e474a0e1aa96614039540cc0b3476cf46d7
Balogh István <istvan.balogh@neuron.hu>
2018-11-20 13:22:52 +0100

    master: https://www.udemy.com/nuxtjs-vuejs-on-steroids: Fix the logout timer

--- store/index.js
+++ store/index.js
@@ -78,21 +78,14 @@ const createStore = () => {
                 }).then(result => {
                     vuexContext.commit('setToken', result.idToken);
 
-                    const expirationDate = new Date().getTime() + result.expiresIn * 1000;
+                    const expirationDate = new Date().getTime() + Number.parseInt(result.expiresIn) * 1000;
 
                     localStorage.setItem('token', result.idToken);
                     localStorage.setItem('tokenExpiration', expirationDate);
 
                     Cookie.set('jwt', result.idToken);
                     Cookie.set('expirationDate', expirationDate);
-
-                    vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000);
                 }).catch(e => console.log(e));
-            },
-            setLogoutTimer(vuexContext, duration) {
-                setTimeout(() => {
-                    vuexContext.commit('clearToken');
-                }, duration);
             },
             initAuth(vuexContext, req) {
                 let token;
@@ -120,13 +113,14 @@ const createStore = () => {
                 } else {
                     token = localStorage.getItem('token');
                     expirationDate = localStorage.getItem('tokenExpiration');
-
-                    if (new Date().getTime() > +expirationDate || !token) {
-                        return;
-                    }
                 }
 
-                vuexContext.dispatch('setLogoutTimer', +expirationDate - new Date().getTime());
+                if (new Date().getTime() > +expirationDate || !token) {
+                    console.log('No token or invalid token');
+                    vuexContext.commit('clearToken');
+                    return;
+                }
+
                 vuexContext.commit('setToken', token);
             }
         },
